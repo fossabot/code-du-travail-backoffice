51ccee353b938791d98369d3c84bea7bb692030f
Ivan Gabriele <ivan.gabriele@gmail.com>
2019-05-15 15:41:16 +0200

    db/migrations/20190515152506_create_questions_suggestions_table: create

---
+++ db/migrations/20190515152506_create_questions_suggestions_table.sql
@@ -0,0 +1,25 @@
+-------------------------------------- UP --------------------------------------
+
+CREATE TABLE api.answers_suggestions(
+  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+
+  question_id uuid NOT NULL REFERENCES api.questions(id),
+  user_id uuid REFERENCES auth.users(id)
+);
+
+CREATE TRIGGER update_updated_at
+  BEFORE UPDATE ON api.answers
+  FOR EACH ROW
+  EXECUTE PROCEDURE set_updated_at();
+
+CREATE CONSTRAINT TRIGGER ensure_user_can_update_answer_suggestion
+  AFTER UPDATE ON api.answers_suggestions
+  FOR EACH ROW
+  EXECUTE PROCEDURE check_user_can_update_answer();
+
+GRANT SELECT, INSERT, DELETE ON api.answers_suggestions TO contributor;
+GRANT SELECT, INSERT, UPDATE, DELETE ON api.answers_suggestions TO administrator;
+
+------------------------------------- DOWN -------------------------------------
+
+DROP TABLE api.answers_suggestions;
